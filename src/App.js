93b27fad433bc397ee80e4dc9840b9b50e028896
Blijonas <blijonasq@gmail.com>
2024-02-10 21:51:25 +0200

    Installed vercel speed-insights

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Experience from "./components/Experience"
 import Contact from "./components/Contact"
 import EndSection from "./components/EndSection"
 import "./App.css"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export default function App() {
     return (
@@ -16,6 +17,7 @@ export default function App() {
             <Experience />
             <Contact />
             <EndSection />
+            <SpeedInsights />
         </main>
     );
 }
